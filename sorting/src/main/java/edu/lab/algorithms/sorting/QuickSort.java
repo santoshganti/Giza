75b2d0f76ca79ecef40f3953c61f0add854d8987
Santosh <santosh.ganti@me.com>
2014-12-05 00:06:44 +0530

    Changes made to pom and quicksort

--- sorting/src/main/java/edu/lab/algorithms/sorting/QuickSort.java
+++ sorting/src/main/java/edu/lab/algorithms/sorting/QuickSort.java
@@ -1,7 +1,9 @@
 package edu.lab.algorithms.sorting;
 
 public class QuickSort {
-	
+	//global variable for the interchange used in the exchange method
+		private int exchangeArray[];
+		
 	//Actual method for performing the quick sort
 	/*
 	 * PSEUDOCODE FOR QUICK SORT METHOD
@@ -10,13 +12,13 @@ public class QuickSort {
 	 * QUICKSORT(A,p,q-1)
 	 * QUICKSORT(A,q+1,r)
 	 */
-	public int[] quickSort(int Array[], int p, int r) {
+	public int[] quickSort(int array[], int p, int r) {
 		if (p < r) {
-			int q = partition(Array, p, r);
-			quickSort(Array, p, q - 1);
-			quickSort(Array, q + 1, r);
+			int q = partition(array, p, r);
+			quickSort(array, p, q - 1);
+			quickSort(array, q + 1, r);
 		}
-		return Array;
+		return array;
 	}
 
 	//Partitions the algorithm into sorted two sets of sub arrays
@@ -31,11 +33,11 @@ public class QuickSort {
 	 * 		exchage A[i+1] with A[r]
 	 * return i+1
 	 */
-	public int partition(int Array[], int p, int r) {
-		int x = Array[r];
+	public int partition(int array[], int p, int r) {
+		int x = array[r];
 		int i = p - 1;
 		for (int j = p; j <= r - 1; j++) {
-			if (Array[j] <= x) {
+			if (array[j] <= x) {
 				i = i + 1;
 				exchange(i, j);
 			}
@@ -45,15 +47,14 @@ public class QuickSort {
 
 	}
 
-	//global variable for the interchange used in the exchange method
-	private int ExchangeArray[];
+	
 
 	//Method to interchange elements
 	private void exchange(int a, int b) {
 		int temp = 0;
-		temp = ExchangeArray[a];
-		ExchangeArray[a] = ExchangeArray[b];
-		ExchangeArray[b] = temp;
+		temp = exchangeArray[a];
+		exchangeArray[a] = exchangeArray[b];
+		exchangeArray[b] = temp;
 
 	}
 
