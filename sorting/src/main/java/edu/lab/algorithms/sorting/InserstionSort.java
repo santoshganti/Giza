0bab22e2c8815c44c8339876f3cdd9a7b5fbc8ed
Santosh <santosh.ganti@me.com>
2014-12-04 22:29:40 +0530

    added comments

--- sorting/src/main/java/edu/lab/algorithms/sorting/InserstionSort.java
+++ sorting/src/main/java/edu/lab/algorithms/sorting/InserstionSort.java
@@ -2,6 +2,16 @@ package edu.lab.algorithms.sorting;
 
 public class InserstionSort {
 
+/*
+ * PSEUDOCODE FOR INSERTION SORT
+ * for j=2 to A.length
+ * 		key=A[j]
+ * 		i=j-1
+ * while i>0 and A[i] >key
+ * 	A[i+1] = A[i]
+ * 	i=i-1
+ * A[i+1] = key
+ */
 	public int[] inserstionSort(int inputarray[]) {
 
 		for (int j = 1; j < inputarray.length; j++) {
