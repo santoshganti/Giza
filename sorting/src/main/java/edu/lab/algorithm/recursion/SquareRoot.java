adeaec98259da512ddd5357edc94d9aac5f97cf6
Santosh <santosh.ganti@me.com>
2014-12-06 22:45:44 +0530

    changed logic in SquareRoot

--- sorting/src/main/java/edu/lab/algorithm/recursion/SquareRoot.java
+++ sorting/src/main/java/edu/lab/algorithm/recursion/SquareRoot.java
@@ -1,23 +1,39 @@
 package edu.lab.algorithm.recursion;
 
+class Seed {
+	private int multiplier;
+	private int exponent = 0;
+	private final int DIVISOR = 100;
+
+	private int calculateMultiplier(double s) {
+		if (s < 100) {
+			return (int) s;
+		} else {
+			exponent++;
+			return calculateMultiplier(s / DIVISOR);
+		}
+
+	}
+
+	Seed(double s) {
+		multiplier = calculateMultiplier(s);
+	}
+
+	public int getExponent() {
+		return exponent;
+	}
+
+	public int getMultiplier() {
+		return multiplier;
+	}
+}
+
 public class SquareRoot {
 
 	private final double EPSILON = 0.15;
 
-	int findExponentForGuess(double a){
-		return 0;
-	}
 	double guess(double s) {
-
-		double k = String.valueOf(s).length();// no. of the digits
-		double n = k - 2; // we raise this as the power
-		double j = k - n;// this gives the value of first two digits
-
-		if (j < 10) {
-			return (2 * Math.pow(10.0, n));
-		} else {
-			return (6 * Math.pow(10.0, n));
-		} 
+		return s;
 
 	}
 
@@ -30,13 +46,12 @@ public class SquareRoot {
 		if (isGoodEnough(guess, s))
 			return guess;
 		else
-		return	squareRoot(improve(guess, s), s);
-		
+			return squareRoot(improve(guess, s), s);
 
 	}
 
 	double improve(double guess, double s) {
-		return (guess + s / guess)/2;
+		return (guess + s / guess) / 2;
 
 	}
 
