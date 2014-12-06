56360b7e58709bb76ed3e2080576d147d019ca68
Santosh <santosh.ganti@me.com>
2014-12-06 14:14:24 +0530

    wrote some but im stuck

---
+++ sorting/src/main/java/edu/lab/algorithm/recursion/SquareRoot.java
@@ -0,0 +1,26 @@
+package edu.lab.algorithm.recursion;
+
+public class SquareRoot {
+	double a;
+	double k = String.valueOf(a).length();// no. of the digits
+	double n = k - 2; // we raise this as the power
+	double j = k - n;// this gives the value of first two digits
+
+	public void roughtApproximation(double a) {
+		if (j < 10) {
+			double S = (2 * Math.pow(10.0, n));
+		} else {
+			double S = (6 * Math.pow(10.0, n));
+		}
+	}
+
+	public int squareRoot(int a) {
+		if (n == 0) {
+			roughtApproximation(a);
+		}
+		else{
+			
+		}
+		return S;
+	}
+}
