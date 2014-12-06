8a4a900cc3f0223546f074e0693cd02077ed2582
Santosh <santosh.ganti@me.com>
2014-12-06 19:38:06 +0530

    added test cases

--- sorting/src/test/java/edu/lab/algorithm/recursion/TestSquareRoot.java
+++ sorting/src/test/java/edu/lab/algorithm/recursion/TestSquareRoot.java
@@ -9,7 +9,12 @@ public class TestSquareRoot {
 	@Test
 	public void testIsGoodEnough() {
 		SquareRoot number = new SquareRoot();
-		assertFalse(600,number.isGoodEnough(125348));
+		assertFalse(number.isGoodEnough(600,125348));
+		assertFalse(number.isGoodEnough(404.457,125348));
+		assertFalse(number.isGoodEnough(354.059,125348));
+		assertFalse(number.isGoodEnough(354.045,125348));
+		assertTrue(number.isGoodEnough(354.045,125348));
+		
 		
 	}
 
