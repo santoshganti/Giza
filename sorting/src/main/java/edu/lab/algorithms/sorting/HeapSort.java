62c1f1ac911fc89851c228b22f768939141fd390
Santosh <santosh.ganti@me.com>
2014-12-07 19:53:34 +0530

    Wrote test cases for heapsort
    Built first level of skeleton

--- sorting/src/main/java/edu/lab/algorithms/sorting/HeapSort.java
+++ sorting/src/main/java/edu/lab/algorithms/sorting/HeapSort.java
@@ -1,5 +1,27 @@
 package edu.lab.algorithms.sorting;
 
 public class HeapSort {
+	public int parent(int i) {
+		return (i / 2);
+	}
 
+	public int left(int i) {
+		return (2 * i);
+	}
+
+	public int right(int i) {
+		return (2 * i + 1);
+	}
+
+	public int[] buildMaxHeap(int[] A) {
+		return A;
+	}
+
+	public int[] maxHeapify(int A[], int i) {
+		return A;
+	}
+
+	public int[] heapSort(int A[]){
+		return A;
+	}
 }
--- sorting/src/test/java/edu/lab/algorithms/sorting/TestHeapSort.java
+++ sorting/src/test/java/edu/lab/algorithms/sorting/TestHeapSort.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 public class TestHeapSort {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHeapSort() {
+		int[] toSort = { 5, 2, 4, 6, 1, 3 };
+		int[] expected = { 1, 2, 3, 4, 5, 6 };
+		HeapSort sort = new HeapSort();
+		int[] sorted = sort.heapSort(toSort);
+		assertArrayEquals(sorted, expected);
 	}
-
 }
