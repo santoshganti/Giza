26438a9e78551888c7055a50d19e9181b9e8a787
Santosh <santosh.ganti@me.com>
2014-12-08 15:47:31 +0530

    Added Data Structures Project

--- sorting/src/main/java/edu/lab/algorithms/sorting/InserstionSort.java
+++ sorting/src/main/java/edu/lab/algorithms/sorting/InserstionSort.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class InserstionSort {
- 
+
 	/*
 	 * PSEUDOCODE FOR INSERTION SORT for j=2 to A.length key=A[j] i=j-1 while
 	 * i>0 and A[i] >key A[i+1] = A[i] i=i-1 A[i+1] = key
@@ -21,15 +21,12 @@ public class InserstionSort {
 			while ((i > -1) && (inputarray[i] > key)) {
 				logger.info("Comparing {} and {}", inputarray[i], key);
 				inputarray[i + 1] = inputarray[i];
+
 				i--;
 			}
 			inputarray[i + 1] = key;
-
 		}
 
 		return inputarray;
-		// logger.info(inputarray);
-
 	}
-
 }
