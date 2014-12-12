9d4c7175ef7fa909f44135efdcfaf514c1e2f392
Santosh Ganti <santosh.ganti@me.com>
2014-12-12 16:44:51 +0530

    static imports

---
+++ lang-java/src/practise/java/edu/lab/algorthms/lang_java/ImportingEntirePackage.java
@@ -0,0 +1,10 @@
+package edu.lab.algorthms.lang_java;
+
+import java.util.*;
+
+public class ImportingEntirePackage {
+	public static void main(String args[]) {
+		ArrayList<String> $a = new ArrayList<String>();
+		TreeSet<String> _a1 = new TreeSet<String>();
+	}
+}
---
+++ lang-java/src/practise/java/edu/lab/algorthms/lang_java/StaticImportsExample.java
@@ -0,0 +1,11 @@
+package edu.lab.algorthms.lang_java;
+
+import static java.lang.System.out;
+import static java.lang.Integer.*;
+
+public class StaticImportsExample {
+	public static void main(String[] args) {
+		out.println(MAX_VALUE);
+		out.println(toHexString(42));
+	}
+}
