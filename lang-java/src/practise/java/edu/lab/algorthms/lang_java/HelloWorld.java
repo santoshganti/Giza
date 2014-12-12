a399eb05a360feb77bf1f1b44437e9dab7bad947
Santosh Ganti <santosh.ganti@me.com>
2014-12-12 15:34:28 +0530

    changes

--- lang-java/src/practise/java/edu/lab/algorthms/lang_java/HelloWorld.java
+++ lang-java/src/practise/java/edu/lab/algorthms/lang_java/HelloWorld.java
@@ -2,7 +2,7 @@ package edu.lab.algorthms.lang_java;
 
 public class HelloWorld {
 	//Testing different types arguments for main  
-	 static public void main(String[]bang_a_gong){
+	 static public void main(String[]kingkong){
 	System.out.print("Hello World");
 	}
 }
