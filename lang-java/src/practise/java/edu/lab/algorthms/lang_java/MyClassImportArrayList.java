d727dc1282c928d38612a9eaac0b9d37d0348dd9
Santosh Ganti <santosh.ganti@me.com>
2014-12-12 15:40:53 +0530

    Practise classes for "imports"

---
+++ lang-java/src/practise/java/edu/lab/algorthms/lang_java/MyClassImportArrayList.java
@@ -0,0 +1,9 @@
+package edu.lab.algorthms.lang_java;
+
+import java.util.ArrayList;
+
+public class MyClassImportArrayList {
+	public static void main(String args[]) {
+		java.util.ArrayList<String> a = new java.util.ArrayList<String>();
+	}
+}
---
+++ lang-java/src/practise/java/edu/lab/algorthms/lang_java/MyClassWithoutImportingArrayList.java
@@ -0,0 +1,8 @@
+package edu.lab.algorthms.lang_java;
+
+//Class without importing arraylist from java.util.
+public class MyClassWithoutImportingArrayList {
+	public static void main(String args[]) {
+		java.util.ArrayList<String> a = new java.util.ArrayList<String>();
+	}
+}
