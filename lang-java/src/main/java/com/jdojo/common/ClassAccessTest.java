5f7353fe363c8df38410deb66b39240263cd9a80
santoshganti <santosh.ganti@me.com>
2014-12-13 12:29:40 -0800

    New class to test access level for classess

---
+++ lang-java/src/main/java/com/jdojo/common/ClassAccessTest.java
@@ -0,0 +1,16 @@
+package com.jdojo.common;
+
+/*
+ * Class can have only two access level modifiers 
+ * 1. No level i.e default -> can be accessed by classes 
+ * from the same package but not other packages.
+ * 
+ * 2. Public -> Any class can access this 
+ */
+
+
+public class ClassAccessTest {
+	public static void main(String[] args) {
+		edu.lab.algorthms.lang_java.Human jack;
+	}
+}
