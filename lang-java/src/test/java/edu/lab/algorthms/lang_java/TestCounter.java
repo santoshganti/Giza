c23d082bde355d25e7f9984d82e9fc34ae476121
Santosh Ganti <santosh.ganti@me.com>
2014-12-13 18:06:34 +0530

    testCase

---
+++ lang-java/src/test/java/edu/lab/algorthms/lang_java/TestCounter.java
@@ -0,0 +1,18 @@
+package edu.lab.algorthms.lang_java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCounter {
+	Counter c1 = new Counter();
+	Counter c2 = new Counter();
+	@Test
+	public void testIncrement() {
+		assertEquals(1,c1.increment());
+		assertEquals(1,c1.staticIncrement());
+		
+	}
+
+
+}
