be357188ac36a045839118d577f06028ce659bae
santoshganti <santosh.ganti@me.com>
2014-12-13 10:15:01 -0800

    added human class to understand more about classes

---
+++ lang-java/src/main/java/edu/lab/algorthms/lang_java/Human.java
@@ -0,0 +1,12 @@
+package edu.lab.algorthms.lang_java;
+
+public class Human {
+	// class variable should be declared using the word static. Class variable
+	// is also known as static variable
+	static long count;
+	// Every instance or object of this class will have these two fields
+	// Instance variable is also known as non-static varible. Objects will have
+	// a copy of instance variables in them
+	String gender;
+	String name;
+}
