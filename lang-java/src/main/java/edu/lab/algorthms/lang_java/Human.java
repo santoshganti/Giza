e0bc03d95cda59c10086ff0bb111323489c8092b
santoshganti <santosh.ganti@me.com>
2014-12-13 11:29:33 -0800

    More fucntionality added to human class

--- lang-java/src/main/java/edu/lab/algorthms/lang_java/Human.java
+++ lang-java/src/main/java/edu/lab/algorthms/lang_java/Human.java
@@ -10,7 +10,36 @@ public class Human {
 	String gender;
 	String name;
 
-	Human Jack = new Human();// Jack will store the memeory location on the
-								// instance of this human
-	
+	@SuppressWarnings("unused")
+	public static void main(String args[]) {
+		Human Jack = new Human();// Jack will store the memeory location on the
+									// instance of this human
+
+		/*
+		 * Assign Null value to John null is a reference type and it can be used
+		 * to assign to reference variable
+		 * 
+		 * A reference variable holding null type will not point to any object.
+		 * It just means null
+		 */
+		Human john = null;
+
+		john = new Human(); // Now john is pointing to proper object
+
+		if (john == null) {
+			/*
+			 * John is referring to null. Cannot use john for anything
+			 */
+		}
+		if (john != null) {
+			/*
+			 * John is not null do something with john
+			 */
+		}
+		/*
+		 * Reference variables can be used to access instance variables because
+		 * they cannot be accessed directly
+		 */
+		
+	}
 }
