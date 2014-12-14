d41e65920189f9bad5faa917dc9f1bf9d748936b
santoshganti <santosh.ganti@me.com>
2014-12-14 17:56:13 +0530

    new class

--- lang-java/src/main/java/greetings/Ferari.java
+++ lang-java/src/main/java/greetings/Ferari.java
@@ -4,22 +4,5 @@ import edu.lab.algorthms.lang_java.Car;
 
 public class Ferari extends Car {
 
-	@Override
-	public void goFast() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void goUpHill() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void impressNeightbors() {
-		// TODO Auto-generated method stub
-		
-	}
 	
 }
