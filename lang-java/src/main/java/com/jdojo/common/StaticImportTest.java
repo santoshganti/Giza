26f29082829fd048faa95c8ee2dc3e2d20463a4d
santoshganti <santosh.ganti@me.com>
2014-12-14 15:41:41 +0530

    Static import test

---
+++ lang-java/src/main/java/com/jdojo/common/StaticImportTest.java
@@ -0,0 +1,14 @@
+package com.jdojo.common;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImportTest {
+	public static void main(String args[]) {
+		double radius = 2.9;
+		double area = PI * radius * radius;
+		out.println("The value of pi is: " + PI);
+		out.println("The value of radius is: " + radius);
+		out.println("The area of the circle is: " + area);
+	}
+}
