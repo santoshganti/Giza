6a8085a835c16427a768fb92e24bb91accb7fcb9
Santosh Ganti <santosh.ganti@me.com>
2014-12-16 14:43:14 +0530

    Starting to refactor Packages

---
+++ lang-java/src/main/java/edu/lab/algorthms/lang_java/CoffeeSize1.java
@@ -0,0 +1,19 @@
+package edu.lab.algorthms.lang_java;
+
+/*
+ * Conceptual Example of how you can think about enums
+ */
+public class CoffeeSize1 {
+	public static final CoffeeSize1 BIG = new CoffeeSize1("BIG", 0);
+	public static final CoffeeSize1 HUGE = new CoffeeSize1("HUGE", 1);
+	public static final CoffeeSize1 OVERWHELMING = new CoffeeSize1(
+			"OVERWHELMING", 2);
+
+	CoffeeSize1(String enumName, int index) {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(CoffeeSize1.BIG);
+	}
+}
--- lang-java/src/main/java/edu/lab/algorthms/lang_java/CoffeeTestEnum.java
+++ lang-java/src/main/java/edu/lab/algorthms/lang_java/CoffeeTestEnum.java
@@ -1,17 +1,33 @@
 package edu.lab.algorthms.lang_java;
 
-class Coffee {
-	enum CoffeeSize {
-		BIG, HUGE, OVERWHELMING
-	};
+enum CoffeeSize {
+	BIG(8), HUGE(10), OVERWHELMING(16);
+	private int ounce;
 
-	CoffeeSize size;
-}
+	CoffeeSize(int ounce) {
+		this.ounce = ounce;
+	}
+
+	private int ounces;
+
+	public int getOunces() {
+		return ounce;
+	}
+};
 
 public class CoffeeTestEnum {
+	CoffeeSize size;
 
 	public static void main(String[] args) {
-		Coffee drink = new Coffee();
-		drink.size = Coffee.CoffeeSize.BIG;
+		CoffeeTestEnum drink1 = new CoffeeTestEnum();
+		drink1.size = CoffeeSize.BIG;
+
+		CoffeeTestEnum drink2 = new CoffeeTestEnum();
+		drink2.size = CoffeeSize.OVERWHELMING;
+
+		System.out.println(drink1.size.getOunces());
+		for (CoffeeSize cs : CoffeeSize.values())
+			System.out.println(cs + " " + cs.getOunces());
+
 	}
 }
