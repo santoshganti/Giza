94960ebf0711a98a957d9f5e27f7e966407488e2
santoshganti <santosh.ganti@me.com>
2014-12-18 22:46:09 +0530

    changes overloading constructors added

--- lang-java/src/main/java/edu/ocaguide/overloading/Animal.java
+++ lang-java/src/main/java/edu/ocaguide/overloading/Animal.java
@@ -1,8 +1,27 @@
 package edu.ocaguide.overloading;
 
 public class Animal {
-	public void eat(){
-		System.out.println("Generic Animal Eating genrically");
-		
+	String name;
+
+	Animal(String name) {
+		this.name = name;
+	}
+
+	Animal() {
+		this(makeRandomName());
+	}
+
+	static String makeRandomName() {
+		int x = (int) (Math.random() * 5);
+
+		String name = new String[] { "Flufy", "Fido", "Rover", "Spike", "Gigi" }[x];
+		return name;
+	}
+
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		System.out.println(a.name);
+		Animal b = new Animal();
+		System.out.println(b.name);
 	}
 }
