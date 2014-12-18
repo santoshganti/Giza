ccb7543876f7dcb5b4da55a1a4a302de74bdc882
santoshganti <santosh.ganti@me.com>
2014-12-18 22:53:44 +0530

    chnages

--- lang-java/src/main/java/edu/ocaguide/overloading/Animal.java
+++ lang-java/src/main/java/edu/ocaguide/overloading/Animal.java
@@ -21,7 +21,7 @@ public class Animal {
 	public static void main(String[] args) {
 		Animal a = new Animal();
 		System.out.println(a.name);
-		Animal b = new Animal();
+		Animal b = new Animal("zeus");
 		System.out.println(b.name);
 	}
 }
