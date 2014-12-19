793a73acdc3181eac55e003a5729da11c7c028de
santoshganti <santosh.ganti@me.com>
2014-12-19 20:39:34 +0530

    changes

--- lang-java/src/main/java/edu/ocaguide/operators/InstanceOfOperator.java
+++ lang-java/src/main/java/edu/ocaguide/operators/InstanceOfOperator.java
@@ -1,18 +1,20 @@
 package edu.ocaguide.operators;
 
-class A{}
+class A {
+}
+
 public class InstanceOfOperator extends A {
-public static void main(String[] args){
-A myA = new InstanceOfOperator();
-	m2(myA);
-}
+	public static void main(String[] args) {
+		A myA = new InstanceOfOperator();
+		m2(myA);
+	}
 
-public static void m2(A a){
-	if(a instanceof InstanceOfOperator)
-		InstanceOfOperator.doBstuff();
-}
+	public static void m2(A a) {
+		if (a instanceof InstanceOfOperator)
+			InstanceOfOperator.doBstuff();
+	}
 
-public static void doBstuff(){
-	System.out.println("'a' referes to a B ");
-}
+	public static void doBstuff() {
+		System.out.println("'a' referes to a B ");
+	}
 }
