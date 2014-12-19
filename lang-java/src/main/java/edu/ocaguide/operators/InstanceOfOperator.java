fcbeb512bf0a7a4dea50b7c59c30a2f92f3fb1a8
santoshganti <santosh.ganti@me.com>
2014-12-19 20:08:17 +0530

    new class

--- lang-java/src/main/java/edu/ocaguide/operators/InstanceOfOperator.java
+++ lang-java/src/main/java/edu/ocaguide/operators/InstanceOfOperator.java
@@ -1,5 +1,18 @@
 package edu.ocaguide.operators;
 
-public class InstanceOfOperator {
+class A{}
+public class InstanceOfOperator extends A {
+public static void main(String[] args){
+A myA = new InstanceOfOperator();
+	m2(myA);
+}
 
+public static void m2(A a){
+	if(a instanceof InstanceOfOperator)
+		InstanceOfOperator.doBstuff();
+}
+
+public static void doBstuff(){
+	System.out.println("'a' referes to a B ");
+}
 }
