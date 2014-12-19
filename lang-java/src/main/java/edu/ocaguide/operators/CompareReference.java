37680ea91b3c3aa9bf6a36af4db2fd438e4e0913
santoshganti <santosh.ganti@me.com>
2014-12-19 19:46:03 +0530

    New class for comparing reference variables

---
+++ lang-java/src/main/java/edu/ocaguide/operators/CompareReference.java
@@ -0,0 +1,14 @@
+package edu.ocaguide.operators;
+
+import javax.swing.JButton;
+
+public class CompareReference {
+	public static void main(String[] args){
+		JButton a = new JButton("Exit");
+		JButton b = new JButton("Exit");
+		JButton c = a;
+		System.out.println("Is reference a === b ? "+(a==b));
+		System.out.println("Is reference a === c ? "+(a==c));
+		
+	}
+}
