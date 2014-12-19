a589c4f42748840c970b101677ac68d872b21d64
Santosh Ganti <santosh.ganti@me.com>
2014-12-20 00:11:59 +0530

    Bug fixes

--- lang-java/src/main/java/edu/harnessingjava/chapter2/DataTypeExample.java
+++ lang-java/src/main/java/edu/harnessingjava/chapter2/DataTypeExample.java
@@ -1,20 +1,13 @@
 package edu.harnessingjava.chapter2;
 
-import com.sun.javafx.collections.IntegerArraySyncer;
-
-import edu.harnessingjava.chapter6.Human;
-
 public class DataTypeExample {
-	public static void main(String[] args){
-	int num1 = 1029989998;
-	float num2 = num1;
-	int num3 = (int) num2;
-	
-	System.out.println(num3);
-	Human human = new Human();
-	
-	
-	
-}
+	public static void main(String[] args) {
+		int num1 = 1029989998;
+		float num2 = num1;
+		int num3 = (int) num2;
+
+		System.out.println(num3);
+
+	}
 
 }
