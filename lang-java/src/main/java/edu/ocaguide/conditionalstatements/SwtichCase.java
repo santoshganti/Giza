d22a84d8e8b05779799608443b6556ce560358b0
Santosh Ganti <santosh.ganti@me.com>
2014-12-20 20:46:59 +0530

    Switch case

---
+++ lang-java/src/main/java/edu/ocaguide/conditionalstatements/SwtichCase.java
@@ -0,0 +1,30 @@
+package edu.ocaguide.conditionalstatements;
+
+enum Fruit {
+	apple, carrot, mango, orange;
+}
+
+public class SwtichCase {
+	public static void main(String[] args) {
+		String value = "apple";
+		Fruit fruit = Fruit.valueOf(value);
+
+		switch (fruit) {
+		case apple:
+			System.out.println("Hi! My name is apple");
+			break;
+
+		case carrot:
+			System.out.println("Hi! My name is apple");
+			break;
+
+		case mango:
+			System.out.println("Hi! My name is apple");
+			break;
+
+		case orange:
+			System.out.println("Hi! My name is apple");
+			break;
+		}
+	}
+}
