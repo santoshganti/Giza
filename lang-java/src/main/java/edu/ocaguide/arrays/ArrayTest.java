90f8e51fb4371eb670c76b6b052a38d5c2ac5908
Santosh Ganti <santosh.ganti@me.com>
2014-12-20 17:13:13 +0530

    new classes

--- lang-java/src/main/java/edu/ocaguide/arrays/ArrayTest.java
+++ lang-java/src/main/java/edu/ocaguide/arrays/ArrayTest.java
@@ -3,19 +3,23 @@ package edu.ocaguide.arrays;
 //Trying to get rows and column for matrix. 
 public class ArrayTest {
 	public static void main(String[] args) {
-		int[][] firstarray = new int[][] { { 2, 3, 5 }, { 4, 5, 5 } };
-		int[][] secondarray = new int[][] { { 1, 2 }, { 2, 3 }, { 3, 4 } };
+		int[][] firstarray = new int[][] { { 1, 1, 1 }, { 1, 1, 1 } };
+		//int[][] secondarray = new int[][] { { 1, 1 }, { 1, 1 }, { 1, 1 } };
+		int[][] secondarray = new int[][]{{3,3},{3,3}};
 		int r1 = firstarray.length;
 		int c1 = firstarray[1].length;
 		int r2 = secondarray.length;
 		int c2 = secondarray[1].length;
-		System.out.println(r1 + " " + c1 + " " + r2 + " " + c2 + "\n");
+		System.out.println(r2);
+		System.out.println(c2);
+		
+		//System.out.println(r1 + " " + c1 + " " + r2 + " " + c2 + "\n");
 
-		for (int i = 0; i < r1; i++) {
-			for (int j = 0; j < c1; j++)
-				System.out.println(firstarray[i][j] + " ");
-			System.out.println();
-		}
+//		for (int i = 0; i < r1; i++) {
+//			for (int j = 0; j < c1; j++)
+//				System.out.println(firstarray[i][j] + " ");
+//			System.out.println();
+//		}
 
 	}
 }
--- lang-java/src/test/java/edu/ocaguide/arrays/TestMatrixMultiplication.java
+++ lang-java/src/test/java/edu/ocaguide/arrays/TestMatrixMultiplication.java
@@ -8,22 +8,17 @@ public class TestMatrixMultiplication {
 
 	@Test
 	public void testMultiplication() {
-		int[][] firstarray = new int[2][3];
-		firstarray[0][0]=1;
-		firstarray[0][1]=1;
-		firstarray[0][2]=1;
-		firstarray[1][0]=1;
-		firstarray[1][1]=1;
-		firstarray[1][2]=1;
-		int[][] secondarray = new int[3][2];
+		int[][] firstarray = new int[][] { { 1, 1, 1 }, { 1, 1, 1 } };
+		int[][] secondarray = new int[][] { { 1, 1 }, { 1, 1 }, { 1, 1 } };
+		int[][] finalarray = new int[][]{{3,3},{3,3}};
 
 		MatrixMultiplication m = new MatrixMultiplication(firstarray.length,
 				firstarray[1].length, secondarray.length, secondarray[1].length);
-		int[][] finalarray = m.matrixMultiplication(firstarray, secondarray);
+		int[][] tomultiply = m.matrixMultiplication(firstarray, secondarray);
 
-		assertArrayEquals(finalarray, firstarray);
+		assertArrayEquals(finalarray, tomultiply);
 
-		fail("Not yet implemented");
+		
 	}
 
 }
